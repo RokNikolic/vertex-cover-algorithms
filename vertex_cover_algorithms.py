0a7d4dbb5baf0d0f78e7f68d49bc33b9e304926f
Rok Nikolic <rok.nikolic@gmail.com>
2024-04-25 16:14:25 +0200

    compleated natural algo and added graphs

--- vertex_cover_algorithms.py
+++ vertex_cover_algorithms.py
@@ -1,6 +1,7 @@
 # Vertex cover problem algorithms
 
 import time
+import os
 
 
 def read_file(name):
@@ -23,7 +24,24 @@ def naive_algorithm(puzzle_input):
 
 def natural_greedy_algorithm(puzzle_input):
     lines = puzzle_input.split("\n")
+    vertex_dict = {}
+    for edge in lines:
+        if not edge:
+            continue
+        vertices = edge.split(" ")
+        if vertices[0] not in vertex_dict:
+            vertex_dict[vertices[0]] = []
+        if vertices[1] not in vertex_dict:
+            vertex_dict[vertices[1]] = []
+        vertex_dict[vertices[0]].append(vertices[1])
+        vertex_dict[vertices[1]].append(vertices[0])
+
+    sorted_dict = sorted(vertex_dict.items(), key=lambda item: len(item[1]), reverse=True)
+
     cover_vertices = set()
+    for vertex, edges in sorted_dict:
+        if vertex not in cover_vertices and not all(ver in cover_vertices for ver in edges):
+            cover_vertices.add(vertex)
 
     return cover_vertices
 
@@ -42,8 +60,8 @@ def greedy_apx2_algorithm(puzzle_input):
     return cover_vertices
 
 
-if __name__ == "__main__":
-    graph_read = read_file('g01.graph')
+def run_all_algorithms(graph_path):
+    graph_read = read_file(graph_path)
 
     start = time.perf_counter()
     result = naive_algorithm(graph_read)
@@ -51,6 +69,20 @@ if __name__ == "__main__":
     print(f"Naive algorithm result of length: {len(result)} computed in: {end - start :.3} seconds.")
 
     start = time.perf_counter()
+    result = natural_greedy_algorithm(graph_read)
+    end = time.perf_counter()
+    print(f"Natural greedy algorithm result of length: {len(result)} computed in: {end - start :.3} seconds.")
+
+    start = time.perf_counter()
     result = greedy_apx2_algorithm(graph_read)
     end = time.perf_counter()
     print(f"Greedy apx2 algorithm result of length: {len(result)} computed in: {end - start :.3} seconds.")
+
+
+if __name__ == "__main__":
+    graph_list = os.listdir(r'./graphs')
+    graph_list.sort()
+    for graph in graph_list:
+        print(f"Running algorihtms on graph {graph}!")
+        run_all_algorithms(f'./graphs/{graph}')
+    
